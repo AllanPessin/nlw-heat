3e5ee50af5f9d0bec2f66bb9104a2cb52f58401f
Allan Da Silva Pessin <allan.pessin@outlook.com>
2021-10-31 11:32:34 -0300

    recuperando dados do usuário; passando middleware de autenticaçao na rota

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -1,4 +1,4 @@
-import { Request, response, Response } from "express";
+import { Request, Response } from "express";
 import { AuthenticateUserService } from "../services/AuthenticateUserService";
 
 class AuthenticateUserController {
@@ -6,9 +6,12 @@ class AuthenticateUserController {
 
     const { code } = request.body
     const service = new AuthenticateUserService()
-    const result = await service.execute(code)    
-   
-    return response.json(result)
+    try {
+      const result = await service.execute(code)    
+      return response.json(result)
+    } catch(err) {
+      return response.json({ error: err.message })
+    }
   }
 }
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { ensureAuthenticated } from "./middleware/ensureAtuthenticated";
 
 const router = Router()
 
@@ -7,5 +9,6 @@ router.get("/", (request, response) => {
   response.json({message: "Server is up"})
 })
 router.post("/authenticate", new AuthenticateUserController().handle)
+router.post("/messages", ensureAuthenticated, new CreateMessageController().handle)
 
 export { router }
